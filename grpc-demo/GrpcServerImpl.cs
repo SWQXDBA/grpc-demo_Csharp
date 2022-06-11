b4183f25fb7badbf1a09868d0c4483f6ab929283
SWQXDBA <983110853@qq.com>
2022-06-11 22:08:05 +0800

    1

--- grpc-demo/GrpcServerImpl.cs
+++ grpc-demo/GrpcServerImpl.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Grpc.Core;
+using Newtonsoft.Json;
 
 namespace grpc_demo;
 
 public class GrpcServerImpl:Greeter.GreeterBase
 {
+    class JsonMessage
+    {
+        
+      public  string name;
+      public int age;
+    }
     public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
     {
+        var deserializeObject = JsonConvert.DeserializeObject<JsonMessage>(request.Request);
+        Console.WriteLine(deserializeObject.name);
+        Console.WriteLine(deserializeObject.age);
         Console.WriteLine(request.Request);
+        deserializeObject.age++;
         var helloReply = new HelloReply();
-        helloReply.Reply = "from c# reply::" + request.Request;
+        helloReply.Reply = JsonConvert.SerializeObject(deserializeObject);
         return Task.FromResult(helloReply);
 
     }
