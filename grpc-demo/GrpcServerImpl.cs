08655c53b224a7982c6ca39c27aaede14700c67e
SWQXDBA <983110853@qq.com>
2022-06-11 21:48:34 +0800

    Initial commit

---
+++ grpc-demo/GrpcServerImpl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace grpc_demo;
+
+public class GrpcServerImpl:Greeter.GreeterBase
+{
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        Console.WriteLine(request.Request);
+        var helloReply = new HelloReply();
+        helloReply.Reply = "from c# reply::" + request.Request;
+        return Task.FromResult(helloReply);
+
+    }
+}
---
+++ grpc-demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Grpc.Core;
+
+namespace grpc_demo
+{
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Server server = new Server
+            {
+                Services = { Greeter.BindService(new GrpcServerImpl() )},
+                Ports = { new ServerPort("localhost", 9090, ServerCredentials.Insecure) }
+  
+            };
+            server.Start();
+
+            Console.WriteLine("gRPC server listening on port " + 9090);
+            Console.WriteLine("任意键退出...");
+            Console.ReadKey();
+                    
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
